1fe60a6acf52a4571c1f12b5576219ce2ddbf957
BernardoBarc <bbarc67@gmail.com>
2025-11-15 17:24:40 -0300

    Criação do arquivo pedido.js

---
+++ fox-uniformes/Backend/models/pedido.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const pedidoSchema = new mongoose.Schema({
+    nomeCliente: {
+        type: String,
+        required: true
+    },
+    produtoId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Produto',
+        required: true
+    },
+    quantidade: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Pendente', 'Em Progresso', 'Concluído', 'Cancelado'],
+        default: 'Pendente'
+    },
+    preco: {
+        type: Number,
+        required: true
+    },
+    entrega: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Pedido = mongoose.model('Pedido', pedidoSchema);
+
+export default Pedido;
